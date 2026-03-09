c3efe196af3b997b23804bde3168c194476467f8
Paweł Mrocheń <pmrochen@users.noreply.github.com>
2026-03-09 17:06:39 +0100

    Add main header file

---
+++ Imaging.hpp
@@ -0,0 +1,13 @@
+/*
+ *	Name: Imaging
+ *	Author: Pawel Mrochen
+ */
+
+#pragma once
+
+#include "ColorMask.hpp"
+#include "ColorSpace.hpp"
+#include "ChannelOrder.hpp"
+#include "PixelFormat.hpp"
+#include "Color3.hpp"
+#include "Color4.hpp"
